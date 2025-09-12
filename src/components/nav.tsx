24343d7c9100682e8539acb91a6bdc2cdc24e4a2
waamer <108493771+Waamer@users.noreply.github.com>
2025-09-12 01:38:18 -0400

    logo img fix

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -91,7 +91,7 @@ export default function Nav() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <Image src="/FiFiLogo.png" alt="Logo" width={175} height={80} className="h-10 w-auto" />
+            <Image src="/logo.png" alt="Logo" width={175} height={80} className="h-10 w-auto" />
           </div>
           
           {/* Desktop Navigation */}
