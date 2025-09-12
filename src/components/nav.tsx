22ca958fc9d076db9a0eb3796854acceda7958d6
waamer <108493771+Waamer@users.noreply.github.com>
2025-09-12 01:29:35 -0400

    image naming fix

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -91,7 +91,7 @@ export default function Nav() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <Image src="/FiFi Logo.png" alt="Logo" width={175} height={80} className="h-10 w-auto" />
+            <Image src="/FiFiLogo.png" alt="Logo" width={175} height={80} className="h-10 w-auto" />
           </div>
           
           {/* Desktop Navigation */}
